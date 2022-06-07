f0856d34aef179221369bf9ed18d3c8a3b34f13b
Nilushanth Thiruchelvam <nilu.thiru@gmail.com>
2022-06-07 00:53:19 -0400

    organized app.py classes into seperate files.

---
+++ node.py
@@ -0,0 +1,6 @@
+class node:
+    def __init__(self, data=None, companyFounded=None, country=None, next=None):
+        self.data = data
+        self.companyFounded = companyFounded
+        self.country = country
+        self.next = next
